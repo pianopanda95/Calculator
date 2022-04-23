10d6aa35257df1b3825e04642bb08350904c3d1e
pianopanda <kramerfanni95@gmail.com>
2022-04-23 10:29:17 +0200

    edit outlook, remove selfcheck console.log(operands)

--- calculator.js
+++ calculator.js
@@ -64,6 +64,7 @@ function checkStart(check1) {
         error = false;
 }
 
+
 function opButton(text) {
     if (text.length < 4)
         return;
@@ -71,27 +72,27 @@ function opButton(text) {
     let check4 = text.split('');
     nextOperator = check4.pop();
     
-    if (!numbers.includes(check4[check4.length-1])) // ?
+    if (!numbers.includes(check4[check4.length-1]))
         return;
     
     check4 = check4.join('');
     findOperator(check4, operator);
 }
 
+
 function checkOperation(text, operator, startsWithUnary){
+
     const separatorIndex = text.indexOf(`${operator}`, startsWithUnary);
     const check2 = Array.from(text);
     check2[separatorIndex] = '|';
     if (separatorIndex == check2.length -1)
         return;
-    const check3 = check2.join('').split('|');
 
+    const check3 = check2.join('').split('|');
     const operands = check3.map(x => Number(x));
     const a = operands[0];
     const b = operands[1];
    
-    console.log(operands);
-
     if (isNaN(b))
         return errorTrue();
     else if (operator == '/' && b == 0)
@@ -100,23 +101,28 @@ function checkOperation(text, operator, startsWithUnary){
         operate(a, b, operator);
 }
 
+
 function operate(a, b, operator) {
+
     switch(operator) {
         case '+': display.textContent = a + b; break;
         case '-': display.textContent = a - b; break;
         case '/': display.textContent = a / b; break;
         case '*': display.textContent = a * b; break;
     }
+
     if (nextOperator)
        display.textContent += nextOperator;
     setDefaultvalues();
 }
+
 
 function errorTrue() {
     alert('ERROR! Something went wrong.');
     setDefaultvalues();
     display.textContent = '';
 }
+
 
 function setDefaultvalues() {
     startsWithUnary = false;
